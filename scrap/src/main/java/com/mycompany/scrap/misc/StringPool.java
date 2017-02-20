79a2395250a8fe8a0287dfb635ca34f01b8949fd
Ganesh Gowtham G R <ganesh.gowtham@gmail.com>
2017-02-21 01:31:46 +0530

    examples

--- scrap/src/main/java/com/mycompany/scrap/misc/StringPool.java
+++ scrap/src/main/java/com/mycompany/scrap/misc/StringPool.java
@@ -1,5 +1,15 @@
 package com.mycompany.scrap.misc;
 
 public class StringPool {
+	public static void main(String[] args) {
+		/* String pool */
+		String str1 = "hello";
+		String str2 = "hello";
 
+		System.out.println(str1 == str2);
+
+		String str3 = new String("hello");
+		String str4 = new String("hello");
+		System.out.println(str3 == str4);
+	}
 }
