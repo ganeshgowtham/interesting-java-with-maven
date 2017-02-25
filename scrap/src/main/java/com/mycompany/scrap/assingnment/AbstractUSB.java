932fb90695c7d47f30897b622e518846b279d7c9
Ganesh Gowtham G R <ganesh.gowtham@gmail.com>
2017-02-25 10:54:55 +0530

    a

---
+++ scrap/src/main/java/com/mycompany/scrap/assingnment/AbstractUSB.java
@@ -0,0 +1,13 @@
+package com.mycompany.scrap.assingnment;
+
+public class AbstractUSB implements USB {
+
+	@Override
+	public String getConnectionStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}                                                          
+
+
